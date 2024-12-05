053051389f7e2cc4961add20c51e266bcfb43f15
avsroses <ava.wilkinson@student.norwichuni.ac.uk>
2024-12-05 08:45:22 +0000

    distribution

--- p5 js 2D Maths/sketch.js
+++ p5 js 2D Maths/sketch.js
@@ -1,7 +1,34 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(innerWidth, innerHeight);
+  noLoop();
 }
 
 function draw() {
   background(220);
+
+  //Random distribution
+  /*
+  for(let i = 0; i < 100; i++){
+    let x = random(380) + random(380);
+    let y = random(380) + random(380);
+    noStroke();
+    fill(88, 191, 98, 25)
+    square(x, y, 40);
+  } */
+
+  //Gaussian distribution
+  let x;
+  let y;
+  const size = 40;
+  for(let i = 0; i < 100; i++){
+    x = randomGaussian(380, 50)
+    y = randomGaussian(380, 50)
+    noStroke();
+    fill(88, 191, 98, 25)
+    square(x, y, size);
+    x = randomGaussian(380, 150)
+    y = randomGaussian(380, 150)
+    fill(35, 46, 145, 25)
+    square(x, y, size);
+  }
 }
